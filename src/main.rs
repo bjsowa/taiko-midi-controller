e0ba47cfc779a330735efdfa0332259207080276
Błażej Sowa <bsowa123@gmail.com>
2025-03-07 18:44:26 +0100

    set clocks

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 
 use defmt::info;
 use embassy_executor::Spawner;
+use embassy_stm32::time::Hertz;
 use embassy_time::Timer;
 
 use {defmt_rtt as _, panic_probe as _};
@@ -13,13 +14,35 @@ embassy_stm32::bind_interrupts!(
     }
 );
 
+fn set_clocks(config: &mut embassy_stm32::Config) {
+    use embassy_stm32::rcc::*;
+    config.rcc.hse = Some(Hse {
+        freq: Hertz(8_000_000),
+        mode: HseMode::Oscillator,
+    });
+    config.rcc.pll = Some(Pll {
+        src: PllSource::HSE,
+        prediv: PllPreDiv::DIV1,
+        mul: PllMul::MUL9,
+    });
+    config.rcc.sys = Sysclk::PLL1_P;
+    config.rcc.ahb_pre = AHBPrescaler::DIV1;
+    config.rcc.apb1_pre = APBPrescaler::DIV2;
+    config.rcc.apb2_pre = APBPrescaler::DIV1;
+}
+
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    let p = embassy_stm32::init(Default::default());
+    let mut stm32_config = embassy_stm32::Config::default();
+    set_clocks(&mut stm32_config);
+
+    let p = embassy_stm32::init(stm32_config);
 
     let driver = embassy_stm32::usb::Driver::new(p.USB, Irqs, p.PA12, p.PA11);
 
-    let config = embassy_usb::Config::new(0xc0de, 0xcafe);
+    let usb_config = embassy_usb::Config::new(0xc0de, 0xcafe);
+
+    // config.rcc.hse = todo!();
 
     let mut config_descriptor = [0; 256];
     let mut bos_descriptor = [0; 256];
@@ -29,7 +52,7 @@ async fn main(_spawner: Spawner) {
 
     let mut builder = embassy_usb::Builder::new(
         driver,
-        config,
+        usb_config,
         &mut config_descriptor,
         &mut bos_descriptor,
         &mut [],
@@ -54,22 +77,4 @@ async fn main(_spawner: Spawner) {
     };
 
     embassy_futures::join::join(usb_fut, echo_fut).await;
-
-    // loop {
-    // midi_class.write_packet(&[0x90, 0x40, 0x7f]).await.unwrap();
-    // }
-
-    // info!("Hello World!");
-
-    // let mut led = Output::new(p.PB12, Level::High, Speed::Low);
-
-    // loop {
-    //     info!("high");
-    //     led.set_high();
-    //     Timer::after_millis(300).await;
-
-    //     info!("low");
-    //     led.set_low();
-    //     Timer::after_millis(300).await;
-    // }
 }
